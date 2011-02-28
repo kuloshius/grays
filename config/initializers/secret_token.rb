5039dc9b1ff32b941c4ecc0f75e43d36b291d3c0
Tomas Lukosius <tomas@ebox.lt>
2011-02-28 22:32:05 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Grays::Application.config.secret_token = '7eaa57315e682592142b67765ee55c0528e6856bbc8c4745e2412750ed7680415b6d2f803a34cdbef528e3d3958c042dccbcbcfd067f7c057c0af39a455a572d'
